44967c78dd9a7b00ce8a9fb0d63216292ecaeb78
denka937 <86137367+denka937@users.noreply.github.com>
2021-07-03 12:35:47 +0900

    Update urls.py
    不要箇所除去

--- queen/urls.py
+++ queen/urls.py
@@ -3,10 +3,8 @@ from . import views
 from .views import topViews,resultViews,detailViews
 
 urlpatterns = [
-    path('', topViews.as_view(), name='top'),
+    path('', views.index,name='top'),
     path('detail/<nameKN>', detailViews.as_view(), name='detail'),
     path('result', views.resultViews, name='result'),
     path('vote/<uuid>', views.vote, name='votecan'),
-    path('', views.index,name='top'),
-
 ]
