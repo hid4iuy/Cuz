1aa76652b3da8448143c830cea7da3695a2c764c
hid4iuy <58177592+hid4iuy@users.noreply.github.com>
2021-07-17 15:47:49 +0900

    開発用DB接続

--- Cuz/settings.py
+++ Cuz/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'Cuz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cuzdb',
+        'USER': 'admin',
+        'PASSWORD': 'cuzdb001',
+        'HOST': 'cuzdb-1.cnwtvecl34rt.us-east-2.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
