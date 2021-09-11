bb40ed02d8a622d014516e836f7cae47e756bc69
EC2 Default User <ec2-user@ip-172-31-1-212.us-east-2.compute.internal>
2021-09-11 14:35:57 +0000

    不要リソース削除

--- Cuz/urls.py
+++ Cuz/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')),
     path('', include('queen.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
